6aed71021e143cd760bec53b2e5053e86d0ce310
aixuefeng <ai_xuefeng@163.com>
2020-01-08 21:27:59 +0800

    新增方法

--- src/sort.java
+++ src/sort.java
@@ -2,4 +2,8 @@ public class sort {
     public static void main(String[] args) {
         System.out.println("djkfhjkhdf");
     }
+
+    public static void so(){
+        System.out.println("123123123456456456789789789552233........");
+    }
 }
